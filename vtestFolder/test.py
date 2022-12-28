e26c8d5c908012de4f6427be75acda5fba209210
jithinrs <jithinrs2.0@gmail.com>
2022-12-28 18:14:13 +0530

    first one

---
+++ vtestFolder/test.py
@@ -0,0 +1,17 @@
+# n = 10
+# for i in range(n):
+#     for j in range(n-i,0,-1):
+#         print("  ", end='')
+#     for j in range(2*i-1):
+#         print("* ", end = '')
+#     print("\n")
+
+# for i in range(n-2):
+#     print("  ", end="")
+
+# print("| | |")
+
+
+import sys
+
+print(sys.getsizeof('മ'))
