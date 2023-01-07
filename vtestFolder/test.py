ccfab3e6d4d6b5861cf4eef3af13cf93f60fa938
jithinrs <jithinrs2.0@gmail.com>
2023-01-07 14:20:29 +0530

    2nd

--- vtestFolder/test.py
+++ vtestFolder/test.py
@@ -15,3 +15,47 @@
 import sys
 
 print(sys.getsizeof('മ'))
+
+
+
+{
+    "first_name": "3535",
+    "last_name": "sdgsdf",
+    "email": "sfsfd@fdf.com",
+    "mobile": "456456",
+    "gender": "M",
+    "date_of_birth": "2022-12-01",
+    "password": "podapatti",
+    "roles": "T",
+    "testerrr": [{
+"nametest":"workata"
+}]
+}
+
+a = 0.1
+b = 0.2
+
+print(a+b)
+
+
+def hello():
+    a = 9+4
+    return None
+
+print(hello())
+class heavy:
+    a = 20
+
+t =heavy()
+hey = t
+t.a = 20
+
+
+
+print((7+3) is (5+5))
+print([] is [])
+helo = []
+hekp1 = []
+
+print(id(helo))
+print(id(hekp1))
